8f199cbec40d91759ca339b943079c9481a59f47
Robert Kuehl <robert.kuehl@iwr.uni-heidelberg.de>
2020-04-29 14:57:16 +0200

    fixed failing test

--- tests/testsMain.cpp
+++ tests/testsMain.cpp
@@ -3,9 +3,9 @@
 //no other purpose than implementing CATCH_CONFIG_MAIN
 //working-directory of the test should be the parent-path of the GigaMesh-Repository
 
-SCENARIO("simple Failing test")
+SCENARIO("simple not Failing test")
 {
-	REQUIRE(1==0);
+	REQUIRE(1==1);
 }
 
 SCENARIO("simple succeding test")
